caa14e562ec4c17952c59c240d7165378843d9ff
chanspar <qhrn12345@naver.com>
2023-12-29 01:47:05 +0900

    builtins1

---
+++ srcs/builtins/ms_check_builtins.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_check_builtins.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chanspar <chanspar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/29 00:18:21 by chanspar          #+#    #+#             */
+/*   Updated: 2023/12/29 00:51:17 by chanspar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ms_builtins.h"
+
+//대문자도 해야됨?  ECHO
+
+int	ms_check_builtin(char **tk_list)
+{
+	char	*builtin_cmd;
+
+	
+	if (!ms_strncmp(builtin_cmd, "echo", 5))
+
+	else if (!ms_strncmp(builtin_cmd, "cd", 3))
+
+	else if (!ms_strncmp(builtin_cmd, "pwd", 4))
+	
+	else if (!ms_strncmp(builtin_cmd, "export", 7))
+	
+	else if (!ms_strncmp(builtin_cmd, "unset", 6))
+	
+	else if (!ms_strncmp(builtin_cmd, "env", 4))
+	
+	else if (!ms_strncmp(builtin_cmd, "exit", 5))
+
+	else
+		return (0);
+}
