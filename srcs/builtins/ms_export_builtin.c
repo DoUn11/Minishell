f491c3fb0c6cf2a5f4612638e5f459eebd9f8e9f
chanspar <qhrn12345@naver.com>
2024-01-05 06:30:31 +0900

    0105 little modify

--- srcs/builtins/ms_export_builtin.c
+++ srcs/builtins/ms_export_builtin.c
@@ -6,7 +6,7 @@
 /*   By: chanspar <chanspar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 00:20:36 by chanspar          #+#    #+#             */
-/*   Updated: 2024/01/05 02:50:00 by chanspar         ###   ########.fr       */
+/*   Updated: 2024/01/05 05:58:23 by chanspar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,34 +30,58 @@ void	ms_export_no_arg(char **envp)
 	envp_copy = ms_envp_copy(envp);
 	env_size = ms_get_listsize(envp_copy);
 	sort_envp(envp_copy, env_size);
-}
-
-void	sort_envp(char **envp, int env_size)
-{
-	int	i;
-	int	n;
-
-	while (env_size--)
+	while (i < env_size)
 	{
-		i = 0;
-		while (envp[i] && envp[i + 1])
-		{
-			if (ms_strlen(envp[i]) < ms_strlen(envp[i + 1]))
-				n = ms_strlen(envp[i]);
-			else
-				n = ms_strlen(envp[i + 1]);
-			if (ms_strncmp(envp[i], envp[i + 1], n + 1) > 0)
-				swap_string(&envp[i], &envp[i + 1]);
-			i++;
-		}
+
+		i++;
 	}
 }
 
-void	swap_string(char **str1, char **str2)
+void	ms_export_arg(char **tk_list)
 {
-	char	*temp;
+	int	i;
+	int	tk_size;
 
-	temp = *str1;
-	*str1 = *str2;
-	*str2 = temp;
+	i = 1;
+	tk_size = ms_get_listsize(tk_list);
+	while (i < tk_size)
+	{
+		ms_check_key(tk_list[i]);
+		i++;
+	}
 }
+
+void	ms_check_key(char *str)
+{
+	int	i;
+
+	if (str[0] != '-' && ms_isalpha(str[0]) != 1)
+	{
+		exit_status = 1;
+		ms_keyerr_print();
+		return ;
+	}
+	i = 1;
+	while (str[i] && str[i] != '=')
+	{
+		if (ms_isalpha(str[i]) != 1 && ms_isdigit(str[i]) != 1)
+		{
+			exit_status = 1;
+			ms_keyerr_print();
+			return ;
+		}
+		i++;
+	}
+	if (str[i] == 0)
+	{
+		//이미 존재시 그대로 놔두기 
+		ms_export_add();
+	}
+	else if (str[i] == '=')
+	{
+		ms_env_add();
+		ms_export_add();
+	}
+
+}
+
